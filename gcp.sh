a97a69ee50d9198f7d8c3363587fc779ae318611
Tompanepp <tompanepp@gmail.com>
2021-07-14 01:22:51 +0200

    use correct variable and enhance gcp to detect current branch and execute against that

--- gcp.sh
+++ gcp.sh
@@ -2,7 +2,8 @@
 #quick utility to add, commit files and push files
 
 message=$1
-branch=$2
+branch="$(git symbolic-ref HEAD 2>/dev/null)"
+branch_name=${branch##refs/heas/}
 
 git add .
 git push --set-upstream origin $branch_name
