d05ed5990a1c5113a461a412a6a7efbf4c79c934
Praise Tompane <praisetompane@gmail.com>
2022-03-19 04:03:38 +0200

    add separate git commit push to handle adding and pushing individual files AND  git add commit push

---
+++ gbadcph.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+#quick utility to add, commit files and push files
+
+gbadcph() {
+    message=$1
+    branch="$(git symbolic-ref HEAD 2>/dev/null)"
+    branch_name=${branch##refs/heas/}
+
+    git add .
+    git push --set-upstream origin $branch_name
+    git commit -m"$message"
+    git push
+}
