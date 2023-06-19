e27efd752a1b67947ee863aeec3ad199a44398db
Praise Tompane <praisetompane@icloud.com>
2023-06-19 15:28:21 +0200

    fix(docs): clean up README, commit and commit amend script to reflect how I use them in practice.

--- gbadcph.sh
+++ gbadcph.sh
@@ -2,44 +2,38 @@
 #quick utility to add, commit files and push files
 
 # commit message structure:
-    # <type>[optional scope]: <description>
-    # [optional body]
-    # [optional footer(s)]
+# <type>[optional scope]: <description>
+# [optional body]
+# [optional footer(s)]
 
-    # <type> values:
-        # feat – a new feature is introduced with the changes
-        # fix – a bug fix has occurred
-        # chore – changes that do not relate to a fix or feature and don't modify src or test files (for    example updating dependencies)
-        # refactor – refactored code that neither fixes a bug nor adds a feature
-        # docs – updates to documentation such as a the README or other markdown files
-        # style – changes that do not affect the meaning of the code, likely related to code formatting such as white-space, missing semi-colons, and so on.
-        # test – including new or correcting previous tests
-        # perf – performance improvements
-        # ci – continuous integration related
-        # build – changes that affect the build system or external dependencies
-        # revert – reverts a previous commit 
+# <type> values:
+# feat – a new feature is introduced with the changes
+# fix – a bug fix has occurred
+# chore – changes that do not relate to a fix or feature and don't modify src or test files (for    example updating dependencies)
+# refactor – refactored code that neither fixes a bug nor adds a feature
+# docs – updates to documentation such as a the README or other markdown files
+# style – changes that do not affect the meaning of the code, likely related to code formatting such as white-space, missing semi-colons, and so on.
+# test – including new or correcting previous tests
+# perf – performance improvements
+# ci – continuous integration related
+# build – changes that affect the build system or external dependencies
+# revert – reverts a previous commit
 
-# References: 
-    # https://www.freecodecamp.org/news/how-to-write-better-git-commit-messages/
-    # https://www.conventionalcommits.org/en/v1.0.0/
+# References:
+# https://www.freecodecamp.org/news/how-to-write-better-git-commit-messages/
+# https://www.conventionalcommits.org/en/v1.0.0/
 
 # parameters:
-    # type = a type that exists in <type> values enum.
-    # description = a clear description what is changing and why.
-    # days : optional = n days from today to set the commit date to.
-        # defaults to today in git implementation
+# type = a type that exists in <type> values enum.
+# description = a clear description what is changing and why.
+# days : optional = n days from today to set the commit date to.
+# defaults to today in git implementation
 
 # usage example:
-    # gbadcph "feat(test_feature)"" "adding a test feature"
+# gbadcph "feat(test_feature)"" "adding a test feature"
 
 gbadcph() {
-    type=$1
     description=$2
-    days=$3
-
-    if [[ -z "$days" ]]
-        then days=0
-    fi
 
     echo $days
 
@@ -48,6 +42,5 @@ gbadcph() {
 
     git add .
     git push --set-upstream origin $branch_name
-    git commit -m"$type" -m"$description" --date="$days.day.ago"
-    git push
+    git commit -m"$description"
 }
--- install.sh
+++ install.sh
@@ -1,32 +1,33 @@
+#    setup scricpt to install the git_facade module
+#    output:
+#        - installs a .git_facade "module" into your home directory.
+#        - adds this module to your `zsh` environment (~/.zshenv file).
+
 z_shell_environment=~/.zshenv #sub with ~/.bashrc if you use bash
 git_facade=~/.git_facade
 
-if [ -e $git_facade ];
-then
+if [ -e $git_facade ]; then
     echo "deleting current git_facade module"
     rm $git_facade
 fi
 
-for entry in "."/g*
-do
-    echo "" >> $git_facade
-    cat $entry >> $git_facade
-    echo "" >> $git_facade
+for entry in "."/g*; do
+    echo "" >>$git_facade
+    cat $entry >>$git_facade
+    echo "" >>$git_facade
 done
 
-if [ -f "$z_shell_environment" ]; 
-    then
-        if grep git_facade $z_shell_environment
-            then
-                echo "updated git_facade module"
-                echo "did not update .zshenv as $git_facade already installed"
-            else
-                echo "" >> $z_shell_environment
-                echo "source $git_facade" >> $z_shell_environment
-                echo "updated .zshenv"
-        fi
+if [ -f "$z_shell_environment" ]; then
+    if grep git_facade $z_shell_environment; then
+        echo "updated git_facade module"
+        echo "did not update .zshenv as $git_facade already installed"
     else
-        touch $z_shell_environment
-        echo "source $git_facade" >> $z_shell_environment
-        echo "created .zshenv"
+        echo "" >>$z_shell_environment
+        echo "source $git_facade" >>$z_shell_environment
+        echo "updated .zshenv"
+    fi
+else
+    touch $z_shell_environment
+    echo "source $git_facade" >>$z_shell_environment
+    echo "created .zshenv"
 fi
