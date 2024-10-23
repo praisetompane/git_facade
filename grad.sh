b1457333e2d5aa06ab23ade3c3b6a98ee9476762
Praise Tompane <praisetompane@icloud.com>
2024-10-23 09:25:22 +0200

    fix(grad): embed in function to prevent running everytime a terminal is instantiated
    --date=0.day.ago

--- grad.sh
+++ grad.sh
@@ -1,7 +1,10 @@
 #!/bin/zsh
 #quick utility to initialize git repo
-repo_url=$1
-git init
-git commit -m"initial commit" -m"initial setup" 
-git remote add origin $repo_url
-git push -u origin main
+
+grad() {
+    repo_url=$1
+    git init
+    git commit -m"initial commit" -m"initial setup"
+    git remote add origin $repo_url
+    git push -u origin main
+}
