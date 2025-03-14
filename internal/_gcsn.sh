af441fdc848e3390175d775e86d069867333996a
Praise Tompane <praisetompane@icloud.com>
2025-03-14 11:57:18 +0200

    fix: fix history utitlities

---
+++ internal/_gcsn.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh
+# utility to sign old commits after {hash}
+# example: git rebase --exec 'git commit --amend --no-edit -n -S' -i 2055243e72c2e9a6dea827cc7975ad6d157e5f2d
+# References: 
+    # https://superuser.com/questions/397149/can-you-gpg-sign-old-commits
+    # Feller, A. 2022 ZA Sign in Simplify signing Git commits and tags with SSH keys [Workshop] - Git Merge 2022. GitHub 
+
+git rebase --exec 'git commit --amend --no-edit -n -S' -i {hash}
---
+++ internal/_gcsn_keep_date.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+# utility to sign old commits after {hash}
+# example: git -c rebase.instructionFormat='%s%nexec GIT_COMMITTER_DATE="%cD" GIT_AUTHOR_DATE="%aD" git commit --amend --no-edit -n -S' rebase -i f49e7269e907987077b733fc0a3bc7a01bfb3b26
+#          git -c rebase.instructionFormat='%s%nexec GIT_COMMITTER_DATE="%cD" GIT_AUTHOR_DATE="%aD" git commit --amend --no-edit -n -S' rebase -i --root
+# References: 
+    # - https://superuser.com/questions/397149/can-you-gpg-sign-old-commits
+    # - https://stackoverflow.com/questions/41301627/how-to-update-git-commit-author-but-keep-original-date-when-amending
+
+
+git -c rebase.instructionFormat='%s%nexec GIT_COMMITTER_DATE="%cD" GIT_AUTHOR_DATE="%aD" git commit --amend --no-edit -n -S' rebase -i {hash}
+
+git commit --amend --no-edit -n -S {hash}
